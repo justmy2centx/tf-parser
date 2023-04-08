4915c6936382b3d8a7642a0c66875907ce5778f8
Lucky La Torre <luckyguylatorre@gmail.com>
2023-04-08 15:15:21 +0800

    Add tf parser service

---
+++ terraform_parse_service/README.md
@@ -0,0 +1,13 @@
+# Build the Docker image
+docker build -t my-terraform-app .
+
+# Run the Docker container
+docker run -d -p 5000:5000 --env-file=.env my-terraform-app
+
+
+
+REPO_NAME = "justmy2centx/tf-parser"
+
+curl -X POST -H "Content-Type: application/json" -d '{"payload": {"properties": {"aws-region": "eu-west-1", "bucket-name": "tripla-bucket"}}}' http://localhost:5001/terraform
+
+export GITHUB_ACCESS_TOKEN=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
---
+++ terraform_parse_service/app.py
@@ -0,0 +1,64 @@
+import os
+import json
+from flask import Flask, request, jsonify
+from github import Github
+
+app = Flask(__name__)
+
+def create_terraform_file(payload):
+    properties = payload["payload"]["properties"]
+    
+    terraform_file = f"""
+provider "aws" {{
+  region = "{properties['aws-region']}"
+}}
+
+resource "aws_s3_bucket" "example" {{
+  bucket = "{properties['bucket-name']}"
+}}
+
+resource "aws_s3_bucket_public_access_block" "example_public_access_block" {{
+  bucket_name = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}}
+"""
+    return terraform_file
+
+@app.route('/terraform', methods=['POST'])
+def terraform():
+    payload = request.get_json()
+    terraform_file = create_terraform_file(payload)
+
+    GITHUB_ACCESS_TOKEN = os.environ['GITHUB_ACCESS_TOKEN']
+    REPO_NAME = "justmy2centx/tf-parser"
+
+    pr_number = create_branch_and_pr(terraform_file, GITHUB_ACCESS_TOKEN, REPO_NAME)
+
+    return {'status': 'success', 'pr_number': pr_number}, 200
+
+def create_branch_and_pr(terraform_file, access_token, repo_name):
+    g = Github(access_token)
+    repo = g.get_repo(repo_name)
+
+    base_branch = repo.get_branch("main")
+    new_branch_name = f"aws-s3-bucket-pr{repo.get_pulls().totalCount + 1}"
+    new_branch_ref = f"refs/heads/{new_branch_name}"
+
+    repo.create_git_ref(ref=new_branch_ref, sha=base_branch.commit.sha)
+    
+    file_name = "aws_s3_bucket.tf"
+    commit_message = f"Add {file_name}"
+    repo.create_file(file_name, commit_message, terraform_file, branch=new_branch_name)
+
+    pr_title = f"Add {file_name}"
+    pr_body = f"PR for adding {file_name}"
+    pr = repo.create_pull(title=pr_title, body=pr_body, head=new_branch_name, base="main")
+
+    return pr.number
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5005')
