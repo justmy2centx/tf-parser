2943bc3f86e0152e838afa8cd459a9fdd3453498
Lucky <luckyguylatorre@gmail.com>
2023-04-08 16:26:28 +0800

    Add aws_s3_bucket_tripla-041023.tf

---
+++ terraform_s3_buckets/aws_s3_bucket_tripla-041023.tf
@@ -0,0 +1,17 @@
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "tripla-041023"
+}
+
+resource "aws_s3_bucket_public_access_block" "example_public_access_block" {
+  bucket_name = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
