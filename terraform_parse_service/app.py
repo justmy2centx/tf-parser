c9752008bb79fd62f16f8ade2924b155c374bf4b
Lucky La Torre <luckyguylatorre@gmail.com>
2023-04-10 23:03:34 +0800

    clean up

--- terraform_parse_service/app.py
+++ terraform_parse_service/app.py
@@ -5,30 +5,6 @@ from github import Github
 
 app = Flask(__name__)
 
-# def create_terraform_file(payload):
-#     properties = payload["payload"]["properties"]
-#     bucket_name = properties['bucket-name']
-
-#     terraform_file = f"""
-# provider "aws" {{
-#   region = "{properties['aws-region']}"
-# }}
-
-# resource "aws_s3_bucket" "example" {{
-#   bucket = "{bucket_name}"
-# }}
-
-# resource "aws_s3_bucket_public_access_block" "example_public_access_block" {{
-#   bucket_name = aws_s3_bucket.example.id
-
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }}
-# """
-#     return terraform_file, bucket_name
-
 
 def create_terraform_file(payload):
     properties = payload["payload"]["properties"]
